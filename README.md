9ed64ea29a91f7926d1bea25166d5f504e15734d
ken <ken@localhost.localdomain>
2019-11-02 05:19:10 -0600

    Created REST API

---
+++ README.md
@@ -0,0 +1,82 @@
+Lighthouse middleware in expressjs
+=
+
+        npm install
+
+        npm start
+
+
+Open a second terminal
+
+
+        curl -X POST -H "Content-Type:application/json" http://localhost:3000/messages -d '{"text":"https://google.com"}'
+
+
+TOTO: 
+Create a post request to Django database
+Model Important metrics for website audit.
+
+
+PERFORMANCE
+---
+
+- Metrics - 
+First Contentful paint
+
+First Meaningful paint
+
+Speed index
+
+first CPU idle
+
+TIme to Interactive
+
+Max Potential First Input Delay
+
+- Opportunities -
+
+- Diagnostics -
+
+- Passed Audits -
+
+
+ACCESSIBILITY
+---
+
+Opportunities for Improvement
+
+Passed Audits
+
+Audits to Manually check
+
+Not applicable
+
+
+
+BEST PRACTICES
+---
+
+Opportunities for Improvement
+
+Passed audits
+
+
+SEARCH ENGINE OPTIMIZATION
+---
+
+Opportunities for Improvement
+
+Passed Audits
+
+
+PROGRESSIVE WEB APP
+---
+
+Fast and reliable
+
+Installable
+
+PWA Optimized
+
+
+
---
+++ src/index.js
@@ -0,0 +1,81 @@
+// followed a tutorial @ 
+// https://www.robinwieruch.de/node-js-express-tutorial
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import uuidv4 from 'uuid/v4';
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//@license Lighthouse Copyright 2017 Google Inc.
+//Code from Documentation
+//https://github.com/GoogleChrome/lighthouse.git
+
+
+console.log("test started")
+
+function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
+  return chromeLauncher.launch(flags).then(chrome => {
+    flags.port = chrome.port;
+    return lighthouse(url, flags, config).then(results =>
+      chrome.kill().then(() => results
+      ));
+  });
+}
+
+const log = require('lighthouse-logger');
+
+const flags = {
+    logLevel: 'info',
+    chromeFlags: ['--headless'],
+    // Available categories: accessibility, best-practices, performance, pwa, seo.
+    // onlyCategories: ['performance'],
+};
+
+log.setLevel(flags.logLevel);
+//logging ends
+
+//REST API
+// cors to bypass Cross-orgin request error
+app.use(cors())
+
+let messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+  };
+  
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+        id,
+        text: req.body.text,
+    };
+    messages[id] = message;
+    
+    launchChromeAndRunLighthouse(message.text, flags).then(results => {
+        return res.send(results.report)
+    })
+});
+
+app.listen(3000, () => 
+    console.log('app listening to port 3000')
+);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
+
+
